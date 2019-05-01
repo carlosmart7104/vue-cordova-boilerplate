02aa199bc5496097a800e85a6c1b737fbf4e6da3
Carlos Martínez <carlosmart7104@gmail.com>
2019-05-01 16:39:15 -0500

    reiniciando el cache de git

--- platforms/android/app/src/main/assets/www/js/about.210371cc.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a1d1:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],u=e("2877"),c={},i=Object(u["a"])(c,n,s,!1,null,"6b68a91a",null);a["default"]=i.exports}}]);
-//# sourceMappingURL=about.210371cc.js.map
